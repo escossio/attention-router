f7a7f6d2f5d02b09dc868bd64c2ec5556d55f164
Leonardo Escossio <leonardo.escossio@gmail.com>
2026-09-20 19:47:29 -0300

    feat: export neutral email connector foundation

--- attention_router/integrations/__init__.py
+++ attention_router/integrations/__init__.py
@@ -1,5 +1,17 @@
 """Provider adapters that terminate at the neutral Integration Contract."""
 
+from attention_router.integrations.email_connector import (
+    EmailConnector,
+    EmailConnectorError,
+    EmailMessageSnapshot,
+    deterministic_message_ref,
+    snapshot_rfc822_message,
+)
+from attention_router.integrations.http_client import (
+    IntegrationIngressClient,
+    IntegrationIngressClientError,
+    IntegrationIngressReceipt,
+)
 from attention_router.integrations.channel_adapters import (
     AdapterNormalizationError,
     ChannelAdapterOutput,
@@ -13,5 +25,13 @@ __all__ = [
     "ChannelAdapterOutput",
     "ChannelInboundAdapter",
     "EmailNormalizedAdapter",
+    "EmailConnector",
+    "EmailConnectorError",
+    "EmailMessageSnapshot",
+    "IntegrationIngressClient",
+    "IntegrationIngressClientError",
+    "IntegrationIngressReceipt",
     "WhatsAppNormalizedAdapter",
+    "deterministic_message_ref",
+    "snapshot_rfc822_message",
 ]
